f12f9371f625f3f9f56783eda4a8c7b1c19f4eb4
leddkire <leddkire@gmail.com>
2013-12-17 16:56:52 -0430

    Archivos de configuracion para el proyecto de Django

--- src/CLEIDjango/urls.py
+++ src/CLEIDjango/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+#Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^evento/', include('Evento.urls', namespace = "Evento")),
+    url(r'^lugar/', include('Lugar.urls', namespace="Lugar")),
     # Examples:
     # url(r'^$', 'CLEIDjango.views.home', name='home'),
     # url(r'^CLEIDjango/', include('CLEIDjango.foo.urls')),
@@ -13,5 +15,6 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
+   
 )
