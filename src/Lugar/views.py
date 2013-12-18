6910b0af533bd8385050a7dcf815dbe8a8137e52
leddkire <leddkire@gmail.com>
2013-12-18 12:57:00 -0430

    Refactorizar en Lugar.views y editar html para que tenga un mejor contexto.

--- src/Lugar/views.py
+++ src/Lugar/views.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
 
 from Lugar.models import Lugar
-from Evento.forms import EventoForm
+from Evento.forms import EventoFormParaLugar
 from Evento.models import Apertura, Clausura, Taller, Ponencia, CharlaInvitada, EventoSocial
 
 def indice(request):
@@ -22,75 +22,52 @@ def eventosDeLugar(request, lugar_id):
     return HttpResponse("Estas viendo los eventos asignados a este lugar: %s" %lugar_id)
 
 def mostrarFormEvento(request, lugar_id, evento_tipo):
-    form = EventoForm()
+    form = EventoFormParaLugar()
     return render(request, 'Lugar/mostrarFormEvento.html', {'form':form, 'lugar_id':lugar_id, 'evento_tipo':evento_tipo,})
 
 def definirEvento(request, lugar_id):
     return render(request, 'Lugar/definirEvento.html',{'lugar_id':lugar_id})
 
 def asignar(request, lugar_id, evento_tipo):
+    def armarEntidad(lugar, evento):
+        evento.lugar = lugar
+        evento.titulo = form.cleaned_data['titulo']
+        evento.duracion = form.cleaned_data['duracion']
+        evento.fechaIni = form.cleaned_data['fechaIni']
+        evento.horaIni = form.cleaned_data['horaIni']
+        evento.save()
     lugar = get_object_or_404(Lugar, pk=lugar_id)
     if request.method == 'POST':
-        form = EventoForm(request.POST)
+        form = EventoFormParaLugar(request.POST)
         if form.is_valid():
             if(evento_tipo == 'apertura'):
                 evento = Apertura()
-                evento.lugar = lugar
-                evento.titulo = form.cleaned_data['titulo']
-                evento.duracion = form.cleaned_data['duracion']
-                evento.fechaIni = form.cleaned_data['fechaIni']
-                evento.horaIni = form.cleaned_data['horaIni']
-                evento.save()
+                armarEntidad(lugar,evento)
                 return HttpResponseRedirect(reverse('Lugar:detalle', args=(lugar_id)))
             elif(evento_tipo == 'clausura'):
                 evento = Clausura()
-                evento.lugar = lugar
-                evento.titulo = form.cleaned_data['titulo']
-                evento.duracion = form.cleaned_data['duracion']
-                evento.fechaIni = form.cleaned_data['fechaIni']
-                evento.horaIni = form.cleaned_data['horaIni']
-                evento.save()
+                armarEntidad(lugar,evento)
                 return HttpResponseRedirect(reverse('Lugar:detalle', args=(lugar_id)))
             elif(evento_tipo == 'taller'):
                 evento = Taller()
-                evento.lugar = lugar
-                evento.titulo = form.cleaned_data['titulo']
-                evento.duracion = form.cleaned_data['duracion']
-                evento.fechaIni = form.cleaned_data['fechaIni']
-                evento.horaIni = form.cleaned_data['horaIni']
-                evento.save()
+                armarEntidad(lugar,evento)
                 return HttpResponseRedirect(reverse('Lugar:detalle', args=(lugar_id)))
             elif(evento_tipo == 'ponencia'):
                 evento = Ponencia()
-                evento.lugar = lugar
-                evento.titulo = form.cleaned_data['titulo']
-                evento.duracion = form.cleaned_data['duracion']
-                evento.fechaIni = form.cleaned_data['fechaIni']
-                evento.horaIni = form.cleaned_data['horaIni']
-                evento.save()
+                armarEntidad(lugar,evento)
                 return HttpResponseRedirect(reverse('Lugar:detalle', args=(lugar_id)))
             elif(evento_tipo == 'charlaInvitada'):
                 evento = CharlaInvitada()
-                evento.lugar = lugar
-                evento.titulo = form.cleaned_data['titulo']
-                evento.duracion = form.cleaned_data['duracion']
-                evento.fechaIni = form.cleaned_data['fechaIni']
-                evento.horaIni = form.cleaned_data['horaIni']
-                evento.save()
+                armarEntidad(lugar,evento)
                 return HttpResponseRedirect(reverse('Lugar:detalle', args=(lugar_id)))
             elif(evento_tipo == 'eventoSocial'):
                 evento = EventoSocial()
-                evento.lugar = lugar
-                evento.titulo = form.cleaned_data['titulo']
-                evento.duracion = form.cleaned_data['duracion']
-                evento.fechaIni = form.cleaned_data['fechaIni']
-                evento.horaIni = form.cleaned_data['horaIni']
-                evento.save()
+                armarEntidad(lugar,evento)
                 return HttpResponseRedirect(reverse('Lugar:detalle', args=(lugar_id)))
             else:
                 return HttpResponse
         else:
-            form = EventoForm()
+            form = EventoFormParaLugar()
             
     return render(request, 'Lugar/mostrarFormEvento.html', 
                   {'form':form, 
