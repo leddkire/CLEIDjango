810a46be883390a729640eac2e515247fdd41d5c
ani <ani@anis.(none)>
2013-12-20 10:38:32 -0430

    comentar evaluacion del urls de cleidjango porque no esta en evaluacion

--- src/CLEIDjango/urls.py
+++ src/CLEIDjango/urls.py
@@ -11,7 +11,7 @@ urlpatterns = patterns('',
     url(r'^articulo/', include('Articulo.urls', namespace="Articulo")),
     url(r'^persona/', include('Persona.urls', namespace="Persona")),
     url(r'^comite/', include('Comite.urls', namespace="Comite")),
-    url(r'^evaluacion/', include('Evaluacion.urls', namespace="Evaluacion")),
+    #url(r'^evaluacion/', include('Evaluacion.urls', namespace="Evaluacion")),
     # Examples:
     # url(r'^$', 'CLEIDjango.views.home', name='home'),
     # url(r'^CLEIDjango/', include('CLEIDjango.foo.urls')),
