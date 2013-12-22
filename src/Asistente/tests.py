25900d423a7eaf0cff2c5f6b4873a0b251f25c9d
patty <patty@localhost>
2013-12-22 18:40:11 -0430

    pruebas de Asistente e Inscripcion

--- src/Asistente/tests.py
+++ src/Asistente/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from Persona.models import Persona
+from Asistente.models import Asistente
+import unittest
 
 # Create your tests here.
+
+class Test(TestCase):
+
+   def testAsistente(self):
+        p = Persona(correo = "patty@usb.ve", dirpostal = 1090)
+        a = Asistente(persona = p)
+        self.assertEqual(p, a.persona)
