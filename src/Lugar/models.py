52e957b46cbb01774b1563635f6d3db8fc769529
patty <patty@localhost>
2013-12-22 22:28:58 -0430

    cambios para que en el html se lea bien capacidad maxima de lugar

--- src/Lugar/models.py
+++ src/Lugar/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Lugar(models.Model):
     nombre = models.CharField(max_length=100)
     ubicacion = models.CharField(max_length = 200)
-    capacidadMax = models.PositiveIntegerField()
+    capacidadMax = models.PositiveIntegerField('capacidad maxima de personas')
 
     def __unicode__(self):
         return self.nombre
