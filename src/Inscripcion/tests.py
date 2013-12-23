9f9b975c14f7997e1cf7129ef6e35b47d4c585d3
EzeG <eznex7@gmail.com>
2013-12-22 22:57:25 -0430

    Crear test de Inscripcion

--- src/Inscripcion/tests.py
+++ src/Inscripcion/tests.py
@@ -15,3 +15,18 @@ class Test(TestCase):
         self.assertEqual(250, i.tarifa)
         self.assertEqual(date(2013,12,24), i.fechainscripcion)
         self.assertEqual(date(2013,12,31), i.fechatope)
+        
+   def test_InscripcionVista(self):
+        p = Persona(correo = "patty@usb.ve", dirpostal = 1090)
+        p.save()
+        i = Inscripcion(correo = p, tarifa = 250, fechainscripcion = date(2013,12,24), fechatope = date(2013,12,31))
+        i.save()
+        resp = self.client.get('/inscripcion/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTrue('inscripcion' in resp.context)
+        self.assertEqual([inscripcion.pk for inscripcion in resp.context['inscripcion']], [1])
+        inscripcion_1 = resp.context['inscripcion'][0]
+        self.assertEqual(p, inscripcion_1.correo)
+        self.assertEqual(250, inscripcion_1.tarifa)
+        self.assertEqual(date(2013,12,24), inscripcion_1.fechainscripcion)
+        self.assertEqual(date(2013,12,31), inscripcion_1.fechatope)
