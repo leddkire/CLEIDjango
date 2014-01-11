faad17106a2a370da04d3c825f7479df44d3f47e
Dan no Dan <dannodan@ubuntu.(none)>
2014-01-11 14:40:05 -0430

    Comenzar a implementar la interfaz grafica

--- src/CLEIDjango/settings.py
+++ src/CLEIDjango/settings.py
@@ -68,7 +68,8 @@ STATIC_ROOT = ''
 STATIC_URL = '/static/'
 
 # Additional locations of static files
-STATICFILES_DIRS = (
+STATICFILES_DIRS = ( 
+    '/home/dannodan/git/CLEIDjango/src/CLEIDjango/static',
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
@@ -111,6 +112,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/home/dannodan/git/CLEIDjango/src/CLEIDjango/templates',
 )
 
 INSTALLED_APPS = (
